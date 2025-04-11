0c6d6bb506976d513f86e2167ce970c871c07caa
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-10 22:13:02 -0600

    agregando atributos a la clase

--- src/main/java/com/example/microservicios/entity/ProductEntity.java
+++ src/main/java/com/example/microservicios/entity/ProductEntity.java
@@ -1,5 +1,15 @@
 package com.example.microservicios.entity;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(value = "product")//definiendo que sera un documento en mongodb
+@Getter //Añadiendo Getters y Setters
+@Setter
+@NoArgsConstructor //Se pide construir un constructor sin argumentos
 public class ProductEntity {
 
     private String id;
@@ -10,5 +20,5 @@ public class ProductEntity {
 
     private Double unitPrice;
 
-    
+
 }
