d49d6647f537211063c397756343bb43f06f8e12
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-10 22:21:11 -0600

    agregando atributos a la clase

--- src/main/java/com/example/microservicios/controller/ProductController.java
+++ src/main/java/com/example/microservicios/controller/ProductController.java
@@ -1,10 +1,28 @@
 package com.example.microservicios.controller;
 
 
+import com.example.microservicios.entity.ProductEntity;
+import com.example.microservicios.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    //Inyectando dependencia
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductEntity> getAllProducts(){
+    return productRepository.findAll();
+    }
 }
