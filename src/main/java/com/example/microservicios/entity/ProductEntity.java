9ba3266ee636adae13f9acc2df62c00a015f4da0
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-10 22:10:46 -0600

    agregando atributos a la clase

--- src/main/java/com/example/microservicios/entity/ProductEntity.java
+++ src/main/java/com/example/microservicios/entity/ProductEntity.java
@@ -1,4 +1,14 @@
 package com.example.microservicios.entity;
 
 public class ProductEntity {
+
+    private String id;
+
+    private String productName;
+
+    private String productDescription;
+
+    private Double unitPrice;
+
+    
 }
