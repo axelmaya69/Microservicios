a12d15e933f0ba2c7ae3e406110e7a34998bdd66
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-11 21:39:36 -0600

    creando metodo para crrear producto

--- src/main/java/com/example/microservicios/controller/ProductController.java
+++ src/main/java/com/example/microservicios/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.example.microservicios.entity.ProductEntity;
 import com.example.microservicios.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class ProductController {
     return productRepository.findAll();
     }
 
-    public void createProduct(){
-        
+    public void createProduct(@RequestBody ProductEntity productEntity){
+
     }
 }
