39b387b3a0931f1565a2a01843fb25f665e2ee53
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-12 22:04:30 -0600

    eliminando duplicados fuera de microservicio

--- src/main/java/com/example/microservicios/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.microservicios.repository;
-
-import com.example.microservicios.entity.ProductEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
- //heredando los atributos de mongorepository 
-public interface ProductRepository extends MongoRepository<ProductEntity,String> {
-}
