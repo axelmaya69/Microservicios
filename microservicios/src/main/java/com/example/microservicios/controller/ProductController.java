5c7a55cee9b6b58aab2f74b411f645daedd6a2d0
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-12 22:01:02 -0600

    corrgiendo estructura de archivos

---
+++ microservicios/src/main/java/com/example/microservicios/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.microservicios.controller;
+
+
+import com.example.microservicios.entity.ProductEntity;
+import com.example.microservicios.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//notaciones para decir que hara uso de un crud
+@RestController
+@RequestMapping("/api/products") //indica la api personalizada
+public class ProductController {
+
+    //Inyectando dependencia
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK) //usado para evitar usar el ResponseStatus<>
+    public List<ProductEntity> getAllProducts(){
+    return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)//usado para evitar usar el ResponseStatus<>
+    public void createProduct(@RequestBody ProductEntity productEntity){
+    productRepository.save(productEntity);
+    }
+}
