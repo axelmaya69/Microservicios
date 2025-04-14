76f9d737e571c982931136fddc587b6cc8ec80a7
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-13 21:56:29 -0600

    configurandolo como cliente

--- microservicios/src/main/java/com/example/microservicios/MicroserviciosApplication.java
+++ microservicios/src/main/java/com/example/microservicios/MicroserviciosApplication.java
@@ -2,8 +2,10 @@ package com.example.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviciosApplication {
 
 	public static void main(String[] args) {
