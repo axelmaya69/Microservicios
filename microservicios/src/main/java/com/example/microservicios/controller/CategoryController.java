6997e08fe5a4d3dce3edba6c025d8134d69e996c
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-19 22:15:40 -0600

    agregadno configuraciones

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -1,11 +1,15 @@
 package com.example.microservicios.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/categories")
+@Getter
+@Setter
 public class CategoryController {
 
     @Value("${app.testProp}")
