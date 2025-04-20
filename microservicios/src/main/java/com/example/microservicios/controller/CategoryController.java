3ea7d4eb16d9ea45b4c9422f5440d452f287971c
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-19 22:14:06 -0600

    agregadno configuraciones

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -1,9 +1,17 @@
 package com.example.microservicios.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
+
+    @Value("${app.testProp}")
+    private String testProp;
+
+    public String getTestProp(){
+        return this.testProp;
+    }
 }
