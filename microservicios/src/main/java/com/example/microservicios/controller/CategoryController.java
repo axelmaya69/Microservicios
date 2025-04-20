e388e97684c3629c3c658ba462bc645bab43bc34
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-19 22:11:58 -0600

    agregadno notaciones rest

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -1,4 +1,9 @@
 package com.example.microservicios.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
 }
