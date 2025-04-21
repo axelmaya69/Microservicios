c83cb3f9dd73c26773357090d337b939f6cfc0c9
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-20 22:19:48 -0600

    agregando notacion

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.microservicios.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 
     //inyectando el valor
