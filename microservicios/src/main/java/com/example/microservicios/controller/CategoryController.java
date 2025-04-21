94f1f5904a8165e376336932f7b97831a51f33f5
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-20 22:15:32 -0600

    agregando notacion

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.microservicios.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class CategoryController {
     @Value("${app.testProp}")
     private String testProp;
 
-    
+    @GetMapping("test-prop")
     public String getTestProp(){
         return this.testProp;
     }
