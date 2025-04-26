a367bf1c2d5997213b9c17f5c0cb453cb59ac8a6
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-25 21:25:03 -0600

    agregando comentarios

--- microservicios/src/main/java/com/example/microservicios/repository/ProductRepository.java
+++ microservicios/src/main/java/com/example/microservicios/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.example.microservicios.repository;
 
 import com.example.microservicios.entity.ProductEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
- //heredando los atributos de mongorepository
+import org.springframework.stereotype.Repository;
+
+//heredando los atributos de mongorepository
+@Repository
 public interface ProductRepository extends MongoRepository<ProductEntity,String> {
 }
