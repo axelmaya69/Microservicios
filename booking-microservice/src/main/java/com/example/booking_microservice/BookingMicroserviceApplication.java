8c7a44b52968bafbd18bc797929f2fd67d98174b
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-01 21:30:45 -0600

    agregando

--- booking-microservice/src/main/java/com/example/booking_microservice/BookingMicroserviceApplication.java
+++ booking-microservice/src/main/java/com/example/booking_microservice/BookingMicroserviceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class BookingMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingMicroserviceApplication.class, args);
 	}
