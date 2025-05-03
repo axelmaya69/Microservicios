f17d0dc820b2467a71d1c3dcc4f0cf441b0dd947
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-02 22:07:31 -0600

    agregando

--- microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
+++ microservicios/src/main/java/com/example/microservicios/controller/CategoryController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api/categories")
 @Getter
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 //Refresca todos los datos con la notacion @Value para no tener que estar reiniciando
 //la aplicacion
-    
+
 public class CategoryController {
 
     //inyectando el valor
